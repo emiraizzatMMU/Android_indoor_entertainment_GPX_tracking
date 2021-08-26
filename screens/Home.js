9a824e0e1a7808aab4b8b1920366868b1394ca28
emiraizzatabdillah <emir7218@gmail.com>
2021-08-26 23:58:19 +0800

    complete gamepad

--- screens/Home.js
+++ screens/Home.js
@@ -7,9 +7,13 @@ import {
   View,
 } from "react-native";
 import { Accelerometer } from "expo-sensors";
-import { JOYSTICK_SIZE, STEP_SENSITIVITY } from "../utils/AppConstant";
+import {
+  JOYSTICK_SENSITIVITY,
+  JOYSTICK_SIZE,
+  STEP_SENSITIVITY,
+} from "../utils/AppConstant";
 import { AntDesign } from "@expo/vector-icons";
-// import AxisPad from "react-native-axis-pad";
+import AxisPad from "react-native-axis-pad";
 
 class Home extends React.Component {
   state = {
@@ -49,6 +53,9 @@ class Home extends React.Component {
         });
       }
     }
+    if (prevState.dir !== this.state.dir) {
+      console.log("dir: ", this.state.dir);
+    }
   }
 
   componentWillUnmount() {
@@ -56,152 +63,71 @@ class Home extends React.Component {
     this.subscription = null;
   }
 
-  handleDir = (dir) => () => {
-    this.setState(
-      {
-        dir,
-      },
-      () => {
-        console.log("dir:", this.state.dir);
+  handleDir = ({ x, y }) => {
+    let posVal = JOYSTICK_SENSITIVITY;
+    let negVal = JOYSTICK_SENSITIVITY * -1;
+    let dir = "";
+
+    if (x < negVal || x > posVal || y < negVal || y > posVal) {
+      if (x < 0) {
+        if (y > x * -1) {
+          dir = "E";
+        } else if (y < x) {
+          dir = "W";
+        } else {
+          dir = "S";
+        }
+      } else if (x > 0) {
+        if (y < x * -1) {
+          dir = "W";
+        } else if (y > x) {
+          dir = "E";
+        } else {
+          dir = "N";
+        }
       }
-    );
+    }
+
+    this.setState({
+      dir,
+    });
   };
 
   handleMenu = () => {
     console.log("menu");
   };
 
-  handleButtonA = () => {
-    console.log("A");
+  handleButtonAShort = () => {
+    console.log("short A");
   };
 
-  handleButtonB = () => {
-    console.log("B");
+  handleButtonALong = () => {
+    console.log("long A");
+  };
+
+  handleButtonBShort = () => {
+    console.log("short B");
+  };
+
+  handleButtonBLong = () => {
+    console.log("long B");
   };
 
   render() {
     const { step } = this.state;
 
-    const JoystickArea = ({ color, dir }) => {
-      return (
-        <TouchableWithoutFeedback
-          onPressIn={this.handleDir(dir)}
-          onPressOut={this.handleDir("")}
-        >
-          <View
-            style={{ backgroundColor: color, height: "33%", width: "33%" }}
-          />
-        </TouchableWithoutFeedback>
-      );
-    };
-
-    const JoystickSmallArea = ({ color, dir }) => {
-      return (
-        <TouchableWithoutFeedback
-          onPressIn={this.handleDir(dir)}
-          onPressOut={this.handleDir("")}
-        >
-          <View
-            style={{ backgroundColor: color, height: "50%", width: "50%" }}
-          />
-        </TouchableWithoutFeedback>
-      );
-    };
-
     return (
       <View style={styles.container}>
         <View style={styles.joystickContainer}>
-          {/* <AxisPad
-          resetOnRelease={true}
-          autoCenter={true}
-          size={150}
-          handlerSize={75}
-          onValue={({ x, y }) => {
-            // values are between -1 and 1
-            // console.log(x, y);
-          }}
-        ></AxisPad> */}
-          <View
-            style={{
-              height: JOYSTICK_SIZE,
-              width: JOYSTICK_SIZE,
-              borderRadius: JOYSTICK_SIZE / 2,
-              // overflow: "hidden",
-              flexDirection: "row",
-              flexWrap: "wrap",
-              transform: [{ rotate: "45deg" }],
-            }}
-          >
-            <JoystickArea color="blue" dir="N" />
-            <View
-              style={{
-                backgroundColor: "green",
-                height: "33%",
-                width: "33%",
-                flexDirection: "row",
-                flexWrap: "wrap",
-              }}
-            >
-              <JoystickSmallArea color="blue" dir="N" />
-              <JoystickSmallArea color="yellow" dir="E" />
-              <JoystickSmallArea color="blue" dir="N" />
-              <JoystickSmallArea color="yellow" dir="E" />
-            </View>
-            <JoystickArea color="yellow" dir="E" />
-            <View
-              style={{
-                backgroundColor: "pink",
-                height: "33%",
-                width: "33%",
-                flexDirection: "row",
-                flexWrap: "wrap",
-              }}
-            >
-              <JoystickSmallArea color="blue" dir="W" />
-              <JoystickSmallArea color="blue" dir="W" />
-              <JoystickSmallArea color="maroon" dir="S" />
-              <JoystickSmallArea color="maroon" dir="S" />
-            </View>
-
-            <View
-              style={{ backgroundColor: "red", height: "33%", width: "33%" }}
-            />
-            <View
-              style={{
-                backgroundColor: "pink",
-                height: "33%",
-                width: "33%",
-                flexDirection: "row",
-                flexWrap: "wrap",
-              }}
-            >
-              <JoystickSmallArea color="yellow" dir="W" />
-              <JoystickSmallArea color="yellow" dir="W" />
-              <JoystickSmallArea color="white" dir="S" />
-              <JoystickSmallArea color="white" dir="S" />
-            </View>
-            <JoystickArea color="maroon" dir="E" />
-            <View
-              style={{
-                backgroundColor: "black",
-                height: "33%",
-                width: "33%",
-                flexDirection: "row",
-                flexWrap: "wrap",
-              }}
-            >
-              <JoystickSmallArea color="maroon" dir="W" />
-              <JoystickSmallArea color="white" dir="W" />
-              <JoystickSmallArea color="maroon" dir="S" />
-              <JoystickSmallArea color="white" dir="S" />
-            </View>
-
-            <JoystickArea color="white" dir="E" />
-          </View>
+          <AxisPad
+            resetOnRelease={true}
+            autoCenter={true}
+            size={JOYSTICK_SIZE}
+            handlerSize={JOYSTICK_SIZE * (2 / 3)}
+            onValue={this.handleDir}
+          ></AxisPad>
         </View>
-        <View
-          style={{ flex: 2, alignItems: "center", justifyContent: "center" }}
-        >
+        <View style={styles.controlMenuContainer}>
           <View
             style={{
               transform: [{ rotate: "90deg" }],
@@ -213,53 +139,47 @@ class Home extends React.Component {
             <TouchableOpacity onPress={this.handleMenu}>
               <AntDesign
                 name="menufold"
-                size={30}
-                style={{ marginBottom: 15 }}
+                size={40}
+                style={{ marginBottom: 20 }}
               />
             </TouchableOpacity>
-            <Text style={styles.stepText}>Step: {step}</Text>
+            <Text style={styles.stepText}>STEP</Text>
+            <Text style={styles.stepNumber}>{step}</Text>
           </View>
         </View>
-        <View
-          style={{
-            flex: 4,
-            width: "100%",
-            flexDirection: "row",
-            flexWrap: "wrap",
-          }}
-        >
+        <View style={styles.gameButtonContainer}>
           <View
-            style={{
-              width: "50%",
-              height: "50%",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "red",
-            }}
+            style={[
+              styles.gameButton,
+              {
+                backgroundColor: "lightcoral",
+              },
+            ]}
           >
             <TouchableOpacity
-              onPress={this.handleButtonB}
-              style={styles.joystickButtonContainer}
+              onPress={this.handleButtonBShort}
+              onLongPress={this.handleButtonBLong}
+              style={styles.gameButtonTouchable}
             >
-              <Text style={styles.joystickButtonText}>B</Text>
+              <Text style={styles.gameButtonText}>B</Text>
             </TouchableOpacity>
           </View>
-          <View style={{ width: "50%", height: "50%" }} />
-          <View style={{ width: "50%", height: "50%" }} />
+          <View style={styles.gameButton} />
+          <View style={styles.gameButton} />
           <View
-            style={{
-              width: "50%",
-              height: "50%",
-              backgroundColor: "yellow",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
+            style={[
+              styles.gameButton,
+              {
+                backgroundColor: "mediumspringgreen",
+              },
+            ]}
           >
             <TouchableOpacity
-              onPress={this.handleButtonA}
-              style={styles.joystickButtonContainer}
+              onPress={this.handleButtonAShort}
+              onLongPress={this.handleButtonALong}
+              style={styles.gameButtonTouchable}
             >
-              <Text style={styles.joystickButtonText}>A</Text>
+              <Text style={styles.gameButtonText}>A</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -276,23 +196,42 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   joystickContainer: {
-    backgroundColor: "red",
     flex: 4,
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
   },
-  stepText: {
-    fontSize: 15,
+  controlMenuContainer: {
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  joystickButtonContainer: {
+  gameButtonContainer: {
+    flex: 4,
+    width: "100%",
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  stepText: {
+    fontSize: 12,
+  },
+  stepNumber: {
+    fontSize: 21,
+  },
+  gameButton: {
+    width: "50%",
+    height: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gameButtonTouchable: {
     width: "100%",
     height: "100%",
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "white",
   },
-  joystickButtonText: {
+  gameButtonText: {
     fontSize: 50,
     textAlign: "center",
     transform: [{ rotate: "90deg" }],
